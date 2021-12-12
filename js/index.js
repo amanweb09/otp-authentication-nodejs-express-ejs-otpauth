e2bf375fb8434cfb87351fae38b10e7197d65cde
Aman <amankhanna224466@gmail.com>
2021-12-12 13:12:48 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const hamMenu = document.querySelector('div.ham-menu');
+
+hamMenu.addEventListener('click', () => {
+    const sideMenu = document.querySelector('div.side-menu');
+    const bars = document.querySelectorAll('.bars');
+
+    bars.forEach((bar) => {
+        bar.classList.toggle('open')
+    })
+
+    sideMenu.classList.toggle('open');
+})
